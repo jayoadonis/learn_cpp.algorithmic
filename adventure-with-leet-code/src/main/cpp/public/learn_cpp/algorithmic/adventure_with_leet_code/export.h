688785aa751ded9dfb9f5167f32d51bd641b8079
jayoadonis <jayo.adonisraphael@gmail.com>
2026-08-23 16:35:39 +0800

    [fix,docs] cxx compiler in windows correctly handle the cxx 20 modules shared symbols.

--- adventure-with-leet-code/src/main/cpp/public/learn_cpp/algorithmic/adventure_with_leet_code/export.h
+++ adventure-with-leet-code/src/main/cpp/public/learn_cpp/algorithmic/adventure_with_leet_code/export.h
@@ -8,17 +8,25 @@
 #define LEARN_CPP_ALGORITHMIC_ADVENTURE_WITH_LEET_CODE_EXPORT_H
 
 #if defined(_WIN32)
+//REM: Windows (MSVC, MINGW[GCC|CLANG], CYGWIN[GCC|CLANG], MSYS2[GCC|CLANG])
 # if defined(LCXXA_AWLEETCODE_BUILD_STATIC)
 #   define LCXXA_AWLEETCODE_API
 # else
+    //REM: '__cpp_modules' is enable if CXX 20+ Modules
 #   if defined(LCXXA_AWLEETCODE_BUILD_EXPORT)
-#     define LCXXA_AWLEETCODE_API __declspec(dllexport)
-#   else
 #     if defined(__cpp_modules)
-        //REM: If using C++20 modules, 'import' handles linkage natively.
+        //REM: C++20 modules, 'export' handles linkage natively.
 #       define LCXXA_AWLEETCODE_API
 #     else
-        //REM: For C++03/C++17 headers, dllimport is safely preserved.
+        //REM: dllexport is safely preserved.
+#       define LCXXA_AWLEETCODE_API __declspec(dllexport)
+#     endif
+#   else
+#     if defined(__cpp_modules)
+        //REM: C++20 modules, 'import' handles linkage natively.
+#       define LCXXA_AWLEETCODE_API
+#     else
+        //REM: dllimport is safely preserved.
 #       define LCXXA_AWLEETCODE_API __declspec(dllimport)
 #     endif
 #   endif
@@ -26,6 +34,8 @@
 # define LCXXA_AWLEETCODE_API_INTERNAL
 
 #elif defined(__GNUC__) || defined(__clang__)
+//REM: Linux or MacOS (GCC, CLANG)
+//REM: In here does not need to '__cpp_modules' transformation
 # if defined(LCXXA_AWLEETCODE_BUILD_STATIC)
 #   define LCXXA_AWLEETCODE_API
 # else
@@ -34,6 +44,7 @@
 # define LCXXA_AWLEETCODE_API_INTERNAL __attribute__((visibility("hidden")))
 
 #else 
+//REM: Unknown Compiler
 # define LCXXA_AWLEETCODE_API
 # define LCXXA_AWLEETCODE_API_INTERNAL
 #endif
